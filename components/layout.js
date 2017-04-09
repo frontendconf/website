103015284b7176555559870fd3391b70c9d4f480
Thomas Jaggi <thomas.jaggi@unic.com>
2017-04-09 13:54:17 +0200

    Add viewport meta tag

--- components/layout.js
+++ components/layout.js
@@ -13,6 +13,7 @@ const Layout = ({ children, title = 'Frontend Conf', header, currentPage, footer
       <title>{title}</title>
       <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Open+Sans:300,400,400i,700" rel="stylesheet" />
       <link href={config.CDN + '/assets/css/default.css'} rel="stylesheet" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
 
     <Header {...header} />
