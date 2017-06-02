7c1f997eafe38bbb696dcd2143d2e3556eb3cf02
Thomas Jaggi <thomas.jaggi@unic.com>
2017-06-02 22:54:01 +0200

    [Design] Fix sponsors, bump assets

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   API: process.env.API || 'https://api.frontendconf.ch',
   CDN: process.env.CDN || 'https://static.frontendconf.ch',
-  ASSET_VERSION: '?v2',
+  ASSET_VERSION: '?v3',
   GA: 'UA-24147044-1'
 }
