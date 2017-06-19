9083cb32c2179061a33fc0a90f693319da098fbd
Thomas Jaggi <thomas.jaggi@unic.com>
2017-06-19 02:37:58 +0200

    Workaround for sass build not working on now.sh

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ npm start
 
 # Deploy
 
+*NOTE*: There is an issue on now.sh where the webpack build throws a SIGSEGV when trying to build the CSS on the server. To temporarily work around this, the deploy steps run a pre-deploy script making sure the CSS is generated locally and uploaded as is.
+
 ```bash
 # Dev
 npm run deploy-dev
--- next.config.js
+++ next.config.js
@@ -4,68 +4,72 @@ const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 
 const dev = process.env.NODE_ENV !== 'production'
+const predeploy = process.env.PREDEPLOY === 'true'
 
 module.exports = {
   webpack: (config) => {
-    const entryFactory = config.entry
-
     // Handle Sass
-    config.entry = () => {
-      return entryFactory().then((entry) => {
-        entry['default.css'] = './static/sass/default.scss'
+    if (dev || predeploy) {
+      const entryFactory = config.entry
 
-        return entry
+      config.entry = () => {
+        return entryFactory().then((entry) => {
+          entry['default.css'] = './static/sass/default.scss'
+
+          return entry
+        })
+      }
+
+      config.plugins.push(
+        new ExtractTextPlugin('static/css/default.css')
+        // The following config does not work properly when running 'next build'
+        // (targeting a directory outside does not seem to work at all and [name] refers to "common")
+        // new ExtractTextPlugin('../static/css/[name]')
+      )
+
+      config.module.rules.push({
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  autoprefixer({
+                    browsers: ['last 10 versions', 'ie 10']
+                  })
+                ].concat(dev ? [] : [
+                  cssnano()
+                ]),
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       })
     }
-
-    config.plugins.push(
-      new ExtractTextPlugin('static/css/default.css')
-      // The following config does not work properly when running 'next build'
-      // (targeting a directory outside does not seem to work at all and [name] refers to "common")
-      // new ExtractTextPlugin('../static/css/[name]')
-    )
-
-    // config.plugins.push(
-    //   function () {
-    //     this.plugin('done', function (stats) {
-    //       require('fs').writeFileSync('./stats.json', JSON.stringify(stats.toJson(), null, '\t'))
-    //     })
-    //   }
-    // )
-
-    config.module.rules.push({
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                autoprefixer({
-                  browsers: ['last 10 versions', 'ie 10']
-                })
-              ].concat(dev ? [] : [
-                cssnano()
-              ]),
-              sourceMap: true
-            }
-          }, {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      })
-    })
 
     // Env variables
     // config.plugins.push(
     //   new webpack.DefinePlugin({
     //     'process.env.API': JSON.stringify(process.env.API)
     //   })
+    // )
+
+    // Stats
+    // config.plugins.push(
+    //   function () {
+    //     this.plugin('done', function (stats) {
+    //       require('fs').writeFileSync('./stats.json', JSON.stringify(stats.toJson(), null, '\t'))
+    //     })
+    //   }
     // )
 
     return config
