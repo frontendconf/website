7d8ca5ad4f78f9027e9f65aee71783f3d6e28bc1
Sascha Eggenberger <hello@saschaeggenberger.com>
2017-07-20 17:55:46 +0200

    bugfix error

--- components/lead.js
+++ components/lead.js
@@ -53,7 +53,9 @@ class Lead extends Component {
       </div>
     </div> : null
 
-    return <section className={this.props.isHome ? 'intro intro--bg-100' : 'intro intro--' + this.props.title.toLowerCase()}>
+    const type = this.props.title ? this.props.title.toLowerCase() : null
+
+    return <section className={this.props.isHome ? 'intro intro--bg-100' : 'intro intro--' + type}>
         <div className="intro__top-content">
         <div className="grid">
           <div className="grid__inner">
