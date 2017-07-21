a0475bbf6f1fc9c81edd7abc3639a7ab8e8ef611
Thomas Jaggi <thomas.jaggi@unic.com>
2017-07-21 13:37:18 +0200

    Fix formatting

--- components/talk.js
+++ components/talk.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Talk extends Component {
-	formatDate (date) {
+  formatDate (date) {
     return new Date(date).toLocaleDateString('de', {
       day: '2-digit',
       month: 'long',
@@ -11,17 +11,17 @@ class Talk extends Component {
   }
 
   render () {
-  	const date = this.formatDate(this.props.fields.date)
+    const date = this.formatDate(this.props.fields.date)
 
     return <div className="talk">
-			<div className="grid__inner">
-				<div className="col-8">
-					<p className="talk__at">Talk at FEC17</p>
-		      <h2 className="talk__title">{this.props.fields.title}</h2>
-					<h4 className="talk__room">{date} {this.props.fields.fromTime} – {this.props.fields.toTime}, {this.props.fields.room}</h4>
-		      <p className="talk__abstract">{this.props.fields.abstract}</p>
-				</div>
-			</div>
+      <div className="grid__inner">
+        <div className="col-8">
+          <p className="talk__at">Talk at FEC17</p>
+          <h2 className="talk__title">{this.props.fields.title}</h2>
+          <h4 className="talk__room">{date} {this.props.fields.fromTime} – {this.props.fields.toTime}, {this.props.fields.room}</h4>
+          <p className="talk__abstract">{this.props.fields.abstract}</p>
+        </div>
+      </div>
     </div>
   }
 }
@@ -30,8 +30,8 @@ Talk.propTypes = {
   title: PropTypes.string,
   abstract: PropTypes.string,
   date: PropTypes.string,
-	fromTime: PropTypes.string,
-	toTime: PropTypes.string
+  fromTime: PropTypes.string,
+  toTime: PropTypes.string
 }
 
 export default Talk
