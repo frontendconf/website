8fbcc442c45309920a5e7605339c771b584a10e5
Thomas Jaggi <thomas.jaggi@unic.com>
2018-08-20 09:41:48 -0400

    Removed schedule tab select demo

--- components/schedule.js
+++ components/schedule.js
@@ -31,8 +31,7 @@ class Schedule extends Component {
   }
 
   getCurrentTab () {
-    // TODO: Remove demo!
-    const currentDay = '2018-08-31' || dateFormatter.formatDate(new Date(), 'YYYY-MM-DD')
+    const currentDay = dateFormatter.formatDate(new Date(), 'YYYY-MM-DD')
     const currentTab = this.props.schedule.find(item => {
       const date = dateFormatter.parse(item.day, 'dddd, D MMM')
       const day = dateFormatter.formatDate(date, 'YYYY-MM-DD')
